42788aff979d26b0096dfbf3365a2e883675dd1a
Masashi Tanaka <masa@kylab.phys.s.u-tokyo.ac.jp>
2026-04-21 18:17:05 +0900

    run.sh

--- GiBUU_pbarAr/run.sh
+++ GiBUU_pbarAr/run.sh
@@ -3,4 +3,4 @@
 mkdir $1
 cd $1
 sed "s/0.03/$2/g" ../001_antiP_Ar_seed1.job | sed "s/11111/$3/g" | sed "s/-999.9/0.0/g" > job.job
-../../GiBUU.x < job.job > log.log
+~/GiBUU/release/objects/GiBUU.x < job.job > log.log
